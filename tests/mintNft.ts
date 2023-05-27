8d80e81c173f464853bd8bd26b10b988ca0f8cbf
256hax <256hax@gmail.com>
2023-05-27 23:15:08 +0900

    feat: add speed test

--- tests/mintNft.ts
+++ tests/mintNft.ts
@@ -34,16 +34,33 @@ describe('Mint NFT without Nonce', async () => {
     let startTime: number;
     let endTime: number;
 
-    console.log('--- Speed Bottleneck ------------------------------------');
+    console.log('\n/// Speed Check Point ///////////////////////////////////////////');
 
     // ------------------------------------
     //  Create Instruction
     // ------------------------------------
+    ///////////////////////////////////////
+    startTime = 0, endTime = 0; // Init
+    startTime = performance.now();
+    ///////////////////////////////////////
+
+    const latestBlockhash = await connection.getLatestBlockhash()
+
+    ///////////////////////////////////////
+    endTime = performance.now();
+    console.log('Get Latest Block Hash  =>', endTime - startTime, 'ms');
+    ///////////////////////////////////////
+
+    ///////////////////////////////////////
+    startTime = 0, endTime = 0; // Init
+    startTime = performance.now();
+    ///////////////////////////////////////
+
     // The mint needs to sign the transaction, so we generate a new keypair for it.
     const mintKeypair = Keypair.generate();
 
     const operationOptions: OperationOptions = {
-      commitment: 'confirmed',
+      commitment: 'confirmed', // If you fail to create Mint Account, set 'finalized' status.
     };
 
     const uri = 'https://arweave.net/rZyxNClGX937dETjo1Pqd8L02uojj9-xuuzqw3K49po'; // Metadata JSON
@@ -62,13 +79,32 @@ describe('Mint NFT without Nonce', async () => {
       );
 
     // Convert to transaction
-    const latestBlockhash = await connection.getLatestBlockhash()
     const transaction = await transactionBuilder.toTransaction(latestBlockhash)
+
+    ///////////////////////////////////////
+    endTime = performance.now();
+    console.log('Create Instructions    =>', endTime - startTime, 'ms');
+    ///////////////////////////////////////
+
+    ///////////////////////////////////////
+    startTime = 0, endTime = 0; // Init
+    startTime = performance.now();
+    ///////////////////////////////////////
 
     // Partially sign the transaction, as the shop and the mint.
     // The account is also a required signer, but they'll sign it with their wallet after we return it.
     // transaction.partialSign(wallet);
     transaction.sign(payer);
+
+    ///////////////////////////////////////
+    endTime = performance.now();
+    console.log('Sign Transactio        =>', endTime - startTime, 'ms');
+    ///////////////////////////////////////
+
+    ///////////////////////////////////////
+    startTime = 0, endTime = 0; // Init
+    startTime = performance.now();
+    ///////////////////////////////////////
 
     const signature = await sendAndConfirmTransaction(
       connection,
@@ -76,8 +112,17 @@ describe('Mint NFT without Nonce', async () => {
       [payer, mintKeypair]
     );
 
-    console.log('mintKeypair.publicKey =>', mintKeypair.publicKey.toString());
-    console.log('payer.publicKey =>', payer.publicKey.toString());
-    console.log('signature =>', signature);
+    ///////////////////////////////////////
+    endTime = performance.now();
+    console.log('Send Transaction       =>', endTime - startTime, 'ms');
+    ///////////////////////////////////////
+
+    // ------------------------------------
+    //  End Speed Test
+    // ------------------------------------
+    const endTimeTotal = performance.now();
+    console.log('\n/// Speed Test Result ///////////////////////////////////////////');
+    console.log('Entire                 =>', endTimeTotal - startTimeTotal, 'ms');
+    console.log('signature              =>', signature);
   });
 });
