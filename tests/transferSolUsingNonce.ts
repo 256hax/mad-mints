9587bd3f1cdcf636d8db504eac7fbf9744049638
256hax <256hax@gmail.com>
2023-05-27 09:47:31 +0900

    chore: move architecture from Node to Anchor

---
+++ tests/transferSolUsingNonce.ts
@@ -0,0 +1,133 @@
+// Lib
+import * as bs58 from "bs58";
+
+// Solana
+import {
+  Connection,
+  Keypair,
+  PublicKey,
+  SystemProgram,
+  Transaction,
+  LAMPORTS_PER_SOL,
+} from "@solana/web3.js";
+
+// Modules
+import { airdrop } from "../app/modules/airdrop";
+import { createNonceAccount } from "../app/modules/createNonceAccount";
+import { getNonceAccount } from "../app/modules/getNonceAccount";
+import { getSolanaConnection } from "../app/modules/getSolanaConnection";
+
+describe("Transfer SOL using Nonce", async () => {
+  it("Run", async () => {
+    const connection = getSolanaConnection();
+
+    const secretKey = '3u4caiG9kSfRSySL9a17tJBUPHdAMkapQrKQeDmHZ9oQeh6LgSKyZMgoicpp9eqZ1Z41Gzom6iputb8b2i9DJweC';
+    const nonceAccountAuth = Keypair.fromSecretKey(bs58.decode(secretKey));
+
+    const payer = Keypair.generate();
+    const reference = Keypair.generate();
+    const taker = Keypair.generate();
+    let nonceAccount: PublicKey | null;
+    let nonce: string;
+    let signature: string;
+
+    // ------------------------------------
+    //  Airdrop to Fee Payer
+    // ------------------------------------
+    await airdrop(connection, payer.publicKey);
+
+    // ------------------------------------
+    //  Create Nonce Account
+    // ------------------------------------
+    nonceAccount = await createNonceAccount(
+      connection,
+      payer,
+      nonceAccountAuth.publicKey,
+    );
+
+    if (!nonceAccount) throw Error('Nonce Account not found.');
+
+    // ------------------------------------
+    //  Get Nonce
+    // ------------------------------------
+    const nonceAccountInfo = await getNonceAccount(connection, nonceAccount);
+
+    if (!nonceAccountInfo) throw Error('Nonce Account not found.');
+    nonce = nonceAccountInfo.nonce;
+
+    // ------------------------------------
+    //  Start Speed Test
+    // ------------------------------------
+    const startTimeTotal = performance.now();
+    let startTime: number;
+    let endTime: number;
+
+    console.log('--- Bottleneck in Performance ---------------------------');
+
+    // ------------------------------------
+    //  Create Instruction
+    // ------------------------------------
+    let tx = new Transaction();
+
+    // nonce advance must be the first insturction
+    let nonceInstruction = SystemProgram.nonceAdvance({
+      noncePubkey: nonceAccount,
+      authorizedPubkey: nonceAccountAuth.publicKey,
+    });
+
+    // after that, you do what you really want to do, here we append a transfer instruction as an example.
+    let txInstruction = SystemProgram.transfer({
+      fromPubkey: payer.publicKey,
+      toPubkey: taker.publicKey,
+      lamports: LAMPORTS_PER_SOL * 0.01,
+    });
+    // Use for get confirmation status.
+    txInstruction.keys.push(
+      { pubkey: reference.publicKey, isWritable: false, isSigner: false },
+    );
+
+    tx.add(nonceInstruction);
+
+    const executionTimes = 10; // Number of times to add instructions.
+    for (let i = 0; i < executionTimes; i++) {
+      tx.add(txInstruction);
+    }
+
+    // assign `nonce` as recentBlockhash
+    tx.recentBlockhash = nonce;
+    tx.feePayer = payer.publicKey;
+
+    // ------------------------------------
+    //  Sign Transaction
+    // ------------------------------------
+    startTime = 0, endTime = 0; // Init
+    startTime = performance.now();
+
+    tx.sign(
+      payer,
+      nonceAccountAuth
+    );
+
+    endTime = performance.now();
+    console.log('Sign Transaction   =>', endTime - startTime, 'ms');
+
+    // ------------------------------------
+    //  Send Transaction
+    // ------------------------------------
+    startTime = 0, endTime = 0; // Init
+    startTime = performance.now();
+
+    signature = await connection.sendRawTransaction(tx.serialize());
+
+    endTime = performance.now();
+    console.log('Send Transaction   =>', endTime - startTime, 'ms');
+
+    // ------------------------------------
+    //  End Speed Test
+    // ------------------------------------
+    const endTimeTotal = performance.now();
+    console.log('--- Summary ---------------------------------------------');
+    console.log('Entire             =>', endTimeTotal - startTimeTotal, 'ms');
+    console.log('signature          =>', signature);
+  });
+});
---
+++ tests/transferSolWithoutNonce.ts
@@ -0,0 +1,100 @@
+// Solana
+import {
+  Connection,
+  Keypair,
+  SystemProgram,
+  Transaction,
+  LAMPORTS_PER_SOL,
+} from "@solana/web3.js";
+
+// Modules
+import { airdrop } from "../app/modules/airdrop";
+import { getSolanaConnection } from "../app/modules/getSolanaConnection";
+
+describe("Transfer SOL without Nonce", async () => {
+  it("Run", async () => {
+    const connection = getSolanaConnection();
+
+    const payer = Keypair.generate();
+    const reference = Keypair.generate();
+    const taker = Keypair.generate();
+    let signature: string;
+
+    // ------------------------------------
+    //  Airdrop to Fee Payer
+    // ------------------------------------
+    await airdrop(connection, payer.publicKey);
+
+    // ------------------------------------
+    //  Start Speed Test
+    // ------------------------------------
+    const startTimeTotal = performance.now();
+    let startTime: number;
+    let endTime: number;
+
+    console.log('--- Bottleneck in Performance ---------------------------');
+
+    // ------------------------------------
+    //  Get Latest Blockhash
+    // ------------------------------------
+    startTime = 0, endTime = 0; // Init
+    startTime = performance.now();
+
+    const blockHash = await connection.getLatestBlockhash();
+
+    endTime = performance.now();
+    console.log('Get Latest Blockhash   =>', endTime - startTime, 'ms');
+
+    // ------------------------------------
+    //  Create Instruction
+    // ------------------------------------
+    let tx = new Transaction();
+
+    let txInstruction = SystemProgram.transfer({
+      fromPubkey: payer.publicKey,
+      toPubkey: taker.publicKey,
+      lamports: LAMPORTS_PER_SOL * 0.01,
+    });
+    txInstruction.keys.push(
+      { pubkey: reference.publicKey, isWritable: false, isSigner: false },
+    );
+
+    const executionTimes = 10; // Number of times to add instructions.
+    for (let i = 0; i < executionTimes; i++) {
+      tx.add(txInstruction);
+    }
+
+    tx.feePayer = payer.publicKey;
+    tx.recentBlockhash = blockHash.blockhash;
+
+    // ------------------------------------
+    //  Sign Transaction
+    // ------------------------------------
+    startTime = 0, endTime = 0; // Init
+    startTime = performance.now();
+
+    tx.sign(payer);
+
+    endTime = performance.now();
+    console.log('Sign Transaction       =>', endTime - startTime, 'ms');
+
+    // ------------------------------------
+    //  Send Transaction
+    // ------------------------------------
+    startTime = 0, endTime = 0; // Init
+    startTime = performance.now();
+
+    signature = await connection.sendRawTransaction(tx.serialize());
+
+    endTime = performance.now();
+    console.log('Send Transaction       =>', endTime - startTime, 'ms');
+
+    // ------------------------------------
+    //  End Speed Test
+    // ------------------------------------
+    const endTimeTotal = performance.now();
+    console.log('--- Summary ---------------------------------------------');
+    console.log('Entire                 =>', endTimeTotal - startTimeTotal, 'ms');
+    console.log('signature              =>', signature);
+  });
+});
