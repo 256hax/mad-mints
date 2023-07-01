c2a41a7a086e335f965f41544d41abec7c807158
256hax <256hax@gmail.com>
2023-07-02 00:26:10 +0900

    feat: sendAndConfirmTransaction into queue

--- queue/src/pages/api/mintLargeAmountNfts.ts
+++ queue/src/pages/api/mintLargeAmountNfts.ts
@@ -1,3 +1,6 @@
+// Lib
+import * as bs58 from 'bs58';
+
 // Quirrel
 import type { NextApiRequest, NextApiResponse } from 'next';
 import signTransactionQueue from "./queues/signTransaction";
@@ -5,7 +8,6 @@ import signTransactionQueue from "./queues/signTransaction";
 // Solana
 import {
   Keypair,
-  sendAndConfirmTransaction,
 } from '@solana/web3.js';
 
 // Modules
@@ -18,29 +20,30 @@ import { progressBar } from 'mad-mints-packages';
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   const connection = getSolanaConnection();
 
-  const payer = Keypair.generate();
+  // Payer
+  const payerSecretKeyBase58 = process.env.PAYER_SECRET_KEY_BASE58;
+  if (!payerSecretKeyBase58) throw new Error('payerSecretKeyBase58 not found.')
+  const payer = Keypair.fromSecretKey(bs58.decode(payerSecretKeyBase58));
+
+  // Minter(Dummy)
+  const minter = Keypair.generate();
 
   // Metaplex
   const metaplex = getMetaplexConnection(connection, payer);
 
-  const minter = Keypair.generate();
-
-  // Nonce Account creation and minting times.
+  // Nonce Account creation and minting times for demo.
   // e.g. 10K = 10_000
   const numberOfNonceAccounts = 3;
 
   // ------------------------------------
   //  Airdrop in Localnet
   // ------------------------------------
-  await airdrop(connection, payer.publicKey, 100);
+  await airdrop(connection, payer.publicKey, 900);
 
   // ------------------------------------
-  //  Start Speed Test
+  //  Create Transaction
   // ------------------------------------
-  // const startTimeTotal = performance.now();
-
-  console.log('Mint NFTs...');
-
+  // Create NFT instructions for demo.
   for (let i = 0; i < numberOfNonceAccounts; i++) {
     progressBar(i, numberOfNonceAccounts);
 
@@ -60,37 +63,39 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     // Convert to transaction
     const tx = await transactionBuilder.toTransaction(latestBlockhash)
 
-    // Partially sign the transaction, as the shop and the mint.
-    // The account is also a required signer, but they'll sign it with their wallet after we return it.
-    // transaction.partialSign(wallet);
     tx.sign(payer);
 
-    const signature = await sendAndConfirmTransaction(
-      connection,
-      tx,
-      [payer, mintKeypair]
-    );
+    // Serialize the transaction and convert to base64 to return it
+    const serializedTransaction = tx.serialize({
+      // We will need Alice to deserialize and sign the transaction
+      requireAllSignatures: false,
+    });
+    const txBase64 = serializedTransaction.toString('base64');
+
+    // Mint(NFT) KeyPair convert to Base58
+    const mintSecretKeyBase58 = bs58.encode(mintKeypair.secretKey);
+
+    const txPayload = ({
+      // tx,
+      txBase64,
+      mintSecretKeyBase58,
+    });
 
     // ------------------------------------
     //  Enqueue
     // ------------------------------------
     // Docs: https://docs.quirrel.dev/api/queue
     await signTransactionQueue.enqueue(
-      signature,
+      txPayload,
       {
         id: mintKeypair.publicKey.toString(),
         // delay: "10s",
       }
     );
     res.status(200).end();
+    // Note: Can't use res.json.
   }
 
-  // ------------------------------------
-  //  End Speed Test
-  // ------------------------------------
-  // const endTimeTotal = performance.now();
-  // console.log('\n/// Speed Test Results ///////////////////////////');
-  // console.log('Entire                   =>', endTimeTotal - startTimeTotal, 'ms');
-  // console.log('Number of Nonce Accounts =>', numberOfNonceAccounts);
-  // console.log('payer                    =>', payer.publicKey.toString());
+  console.log('Number of Nonce Accounts =>', numberOfNonceAccounts);
+  console.log('payer                    =>', payer.publicKey.toString());
 };
--- queue/src/pages/api/queues/signTransaction.ts
+++ queue/src/pages/api/queues/signTransaction.ts
@@ -1,8 +1,57 @@
-import { Queue } from "quirrel/next";
+// Lib
+import * as bs58 from 'bs58';
+
+// Quirrel
+import { Queue } from 'quirrel/next';
+
+// Solana
+import {
+  Keypair,
+  Transaction,
+  sendAndConfirmTransaction,
+} from '@solana/web3.js';
+
+// Modules
+import { getSolanaConnection } from 'mad-mints-packages';
 
 export default Queue(
   "api/queues/signTransaction", // the route it's reachable under
-  async (recipient: any) => {
-    console.log(`Signature: ${recipient}`);
+  async (recipient: {
+    txBase64: string,
+    mintSecretKeyBase58: string,
+  }) => {
+    // Connection
+    const connection = getSolanaConnection();
+
+    // Validation
+    // txBase64
+    const txBase64 = recipient.txBase64;
+    if (!txBase64) throw new Error('transactionBase64 not found.');
+    // mintSecretKeyBase58
+    const mintSecretKeyBase58 = recipient.mintSecretKeyBase58;
+    if (!mintSecretKeyBase58) throw new Error('mintSecretKeyBase58 not found.');
+
+    // Payer
+    const payerSecretKeyBase58 = process.env.PAYER_SECRET_KEY_BASE58;
+    if (!payerSecretKeyBase58) throw new Error('payerSecretKeyBase58 not found.')
+    const payer = Keypair.fromSecretKey(bs58.decode(payerSecretKeyBase58));
+
+    // Mint(NFT) SecretKey convert to KeyPair
+    const mintKeypair = Keypair.fromSecretKey(bs58.decode(mintSecretKeyBase58));
+
+    // Recover Transaction
+    // The caller of this can convert it back to a transaction object:
+    const recoveredTx = Transaction.from(
+      Buffer.from(txBase64, 'base64')
+    );
+
+    // Send Transaction
+    const signature = await sendAndConfirmTransaction(
+      connection,
+      recoveredTx,
+      [payer, mintKeypair]
+    );
+
+    console.log('signature =>', signature);
   }
 );
